4de6f288800785c572863a3c4e26513d8bc7ba97
focus_ed <mhnaik227@gmail.com>
2026-02-07 19:18:46 +0530

    Files added

---
+++ DAY-2/Task3.py
@@ -0,0 +1,12 @@
+# Hardcoded values
+item_name = "Laptop"
+quantity = 2
+price = 499.99
+in_stock = True
+
+# Print formatted receipt
+print("Item:", item_name, ", Qty:", quantity, ", Price:", price, ", Available:", in_stock)
+
+# Calculate and print total cost
+total_cost = quantity * price
+print("Total Cost:", total_cost)
---
+++ DAY-5/function.py
@@ -0,0 +1,34 @@
+# CALLING A FUNCTION
+# def add(a, b):
+#     result = a + b
+#     return result
+
+# # calling the function
+# x = add(5, 5)
+# print(x)
+
+# -------------------
+# def multiply(a, b):
+#     return a * b
+
+# # calling the function
+# print(multiply(2, 2))
+
+
+
+# -------------------
+# def mul_numbers(a,b):
+#     return a * b
+# result=mul_numbers(5,3)
+# print(result) 
+
+# -------------------
+
+
+
+# def= default argument function
+def cal(a,b): 
+    return a+b,a-b,a*b,a/b
+
+# calling the function
+print(cal(10,5))
